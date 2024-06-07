5d71f2cfe2eb22072c8b0535c066658ef989f385
“Calvin <Calvinli0824@gmail.com>
2024-06-06 23:49:37 -0400

    feat: video can be imported

--- frontend/src/Components/ImportBox.js
+++ frontend/src/Components/ImportBox.js
@@ -1,11 +1,25 @@
 import React, { useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
+import "../styles/Import.css"
 
 export function ImportBox() {
     const onDrop = useCallback((imported) => {
         console.log(imported)
     }, []);
 
-    
-    return;
+    const {getRootProps, getInputProps, isDragActive} = useDropzone(
+        {onDrop,
+        accept:'video/*'
+        })
+
+    return (
+        <div className='drop' {...getRootProps()}>
+            <input {...getInputProps()}/>
+            {isDragActive ? (
+            <p>Drop the video files here ...</p>
+          ) : (
+            <p>Drag 'n' drop some video files here, or click to select files</p>
+      )}
+        </div>
+    )
 }
