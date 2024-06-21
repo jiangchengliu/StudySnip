972c27b5c94c53b0db14d44c4bb6965b1733f56d
“Calvin <Calvinli0824@gmail.com>
2024-06-21 14:43:14 -0400

    feat: video preview added(might delete)

--- frontend/src/Components/ImportBox.js
+++ frontend/src/Components/ImportBox.js
@@ -1,11 +1,26 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import "../styles/Import.css"
 
 export function ImportBox({ open, onClose }) {
     const onDrop = useCallback((imported) => {
-        console.log(imported)
+        imported.forEach((file) => {
+            const reader = new FileReader()
+
+            reader.onabort = () => console.log("reading files aborted")
+            reader.onerror = () => console.log("Error occured while reading files")
+            reader.onload = () => {
+                // for now just print out contents
+                const contents = reader.result
+                console.log(contents)
+            }
+            reader.readAsArrayBuffer(file)
+            const preview = URL.createObjectURL(file)
+            setPreview(preview)
+        })
     }, []);
+
+    const [preview, setPreview] = useState(null)
 
     const {getRootProps, getInputProps, isDragActive} = useDropzone(
         {onDrop,
@@ -26,14 +41,20 @@ export function ImportBox({ open, onClose }) {
                     <button className="close-button" onClick={onClose}>X</button>
                 </div>
                 <div className="dialog-content">
-                    <div className="drop" {...getRootProps()}>
-                        <input {...getInputProps()} />
-                        {isDragActive ? (
-                            <p>Drop the video files here ...</p>
-                        ) : (
-                            <p>Drag 'n' drop some video files here, or click to select files</p>
-                        )}
-                    </div>
+                    {preview ? (
+                        <video width={400} controls>
+                            <source src={preview} type='video/mp4' />
+                        </video>
+                    ) : (
+                        <div className="drop" {...getRootProps()}>
+                            <input {...getInputProps()} />
+                            {isDragActive ? (
+                                <p>Drop the video files here ...</p>
+                            ) : (
+                                <p>Drag 'n' drop some video files here, or click to select files</p>
+                            )}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
